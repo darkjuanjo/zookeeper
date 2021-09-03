21014455aad7a4bb445448e7c02de1dca1f3f64c
Juan Jose Paz <juanjosepazroman@hotmail.com>
2021-09-02 22:48:03 -0700

    finished integrating front-end

--- server.js
+++ server.js
@@ -91,10 +91,6 @@ function filterByQuery(query, animalsArray) {
     return true;
   }
 
-  app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-  });
-
   app.get('/api/animals/:id', (req,res) => {
     const result = findById(req.params.id, animals);
     if(result)
@@ -117,6 +113,22 @@ function filterByQuery(query, animalsArray) {
       const animal = createNewAnimal(req.body, animals);
       res.json(animal);
     }
+  });
+
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+
+  app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+  });
+
+  app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+  });
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
   });
 
 
